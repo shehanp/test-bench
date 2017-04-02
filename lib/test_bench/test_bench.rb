6350329159a9844f7a8339ba2deb2b342bd9bb6b
ntl <nathanladd+github@gmail.com>
2017-04-02 14:28:25 -0500

    Superfluous TestBench namespace is removed

--- lib/test_bench/test_bench.rb
+++ lib/test_bench/test_bench.rb
@@ -4,15 +4,15 @@ module TestBench
       const_set :Assertions, Assert::AssertionsModule::Proc
     end
 
-    settings = TestBench::Settings.new
-    TestBench::Settings::Environment::Set.(settings)
+    settings = Settings.new
+    Settings::Environment::Set.(settings)
 
-    run = TestBench::Run.new
+    run = Run.new
     run.settings = settings
 
     main_object = TOPLEVEL_BINDING.receiver
     main_object.extend TestBench::Structure
 
-    TestBench::Run::Registry.put run, main_object
+    Run::Registry.put run, main_object
   end
 end
